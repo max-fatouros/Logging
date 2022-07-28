ae74fb5c80af9bb1e194db381ad1cdd7a36ad7fc
Max Fatouros <max.fatouros@gmail.com>
2022-07-28 11:20:24 -0500

    ran clang-format

--- Logging.cc
+++ Logging.cc
@@ -3,106 +3,105 @@
 //
 
 #include "Logging.h"
-#include <iostream>
 #include <fstream>
+#include <iostream>
 
 Logging::Logging(int LOG_LEVEL, bool clear) : LOG_LEVEL(LOG_LEVEL) {
-    #ifdef DEBUG
-        this->LOG_LEVEL = Level::eDEBUG;
-    #elif defined(INFO)
-        this->LOG_LEVEL = Level::eINFO;
-    #elif defined(WARNING)
-        this->LOG_LEVEL = Level::eWARNING;
-    #elif defined(ERROR)
-        this->LOG_LEVEL = Level::eERROR;
-    #elif defined(CRITICAL)
-        this->LOG_LEVEL = Level::eCRITICAL;
-    #endif
+#ifdef DEBUG
+  this->LOG_LEVEL = Level::eDEBUG;
+#elif defined(INFO)
+  this->LOG_LEVEL = Level::eINFO;
+#elif defined(WARNING)
+  this->LOG_LEVEL = Level::eWARNING;
+#elif defined(ERROR)
+  this->LOG_LEVEL = Level::eERROR;
+#elif defined(CRITICAL)
+  this->LOG_LEVEL = Level::eCRITICAL;
+#endif
 
-    if (clear) {
-        std::ofstream file(logOut, std::fstream::trunc);
-        file.close();
+  if (clear) {
+    std::ofstream file(logOut, std::fstream::trunc);
+    file.close();
 
-        std::ofstream varFile(logVars, std::fstream::trunc);
-        varFile.close();
-    }
+    std::ofstream varFile(logVars, std::fstream::trunc);
+    varFile.close();
+  }
 }
 
 void Logging::debug(const char *output) const {
-    if (LOG_LEVEL >= Level::eDEBUG) {
-        std::string s;
-        s += "[DEBUG]\t\t";
-        s += output;
-        std::ofstream file(logOut, std::fstream::app);
-        file << s << std::endl;
-        file.close();
-        print(s, COLOR::eGREEN);
-    }
+  if (LOG_LEVEL >= Level::eDEBUG) {
+    std::string s;
+    s += "[DEBUG]\t\t";
+    s += output;
+    std::ofstream file(logOut, std::fstream::app);
+    file << s << std::endl;
+    file.close();
+    print(s, COLOR::eGREEN);
+  }
 }
 
 void Logging::info(const char *output) {
-    if (LOG_LEVEL >= Logging::Level::eINFO) {
-        std::string s;
-        s += "[INFO]\t\t";
-        s += output;
-        std::ofstream file(logOut, std::fstream::app);
-        file << s << std::endl;
-        file.close();
-        print(s, COLOR::eBLUE);
-    }
+  if (LOG_LEVEL >= Logging::Level::eINFO) {
+    std::string s;
+    s += "[INFO]\t\t";
+    s += output;
+    std::ofstream file(logOut, std::fstream::app);
+    file << s << std::endl;
+    file.close();
+    print(s, COLOR::eBLUE);
+  }
 }
 
 void Logging::warning(const char *output) {
-    if (LOG_LEVEL >= Logging::Level::eWARNING) {
-        std::string s;
-        s += "[WARNING]\t";
-        s += output;
-        std::ofstream file(logOut, std::fstream::app);
-        file << s << std::endl;
-        file.close();
-        print(s, COLOR::eYELLOW);
-    }
+  if (LOG_LEVEL >= Logging::Level::eWARNING) {
+    std::string s;
+    s += "[WARNING]\t";
+    s += output;
+    std::ofstream file(logOut, std::fstream::app);
+    file << s << std::endl;
+    file.close();
+    print(s, COLOR::eYELLOW);
+  }
 }
 
 void Logging::error(const char *output) {
-    if (LOG_LEVEL >= Logging::Level::eERROR) {
-        std::string s;
-        s += "[ERROR]\t\t";
-        s += output;
-        std::ofstream file(logOut, std::fstream::app);
-        file << s << std::endl;
-        file.close();
-        print(s, COLOR::eORANGE);
-    }
+  if (LOG_LEVEL >= Logging::Level::eERROR) {
+    std::string s;
+    s += "[ERROR]\t\t";
+    s += output;
+    std::ofstream file(logOut, std::fstream::app);
+    file << s << std::endl;
+    file.close();
+    print(s, COLOR::eORANGE);
+  }
 }
 
 void Logging::critical(const char *output) {
-    if (LOG_LEVEL >= Logging::Level::eCRITICAL) {
-        std::string s;
-        s += "[CRITICAL]\t";
-        s += output;
-        std::ofstream file(logOut, std::fstream::app);
-        file << s << std::endl;
-        file.close();
-        print(s, COLOR::eRED);
-    }
+  if (LOG_LEVEL >= Logging::Level::eCRITICAL) {
+    std::string s;
+    s += "[CRITICAL]\t";
+    s += output;
+    std::ofstream file(logOut, std::fstream::app);
+    file << s << std::endl;
+    file.close();
+    print(s, COLOR::eRED);
+  }
 }
 
 void Logging::log(const char *key, const char *output) {
-    std::ofstream file(logVars, std::fstream::app);
-    file << "[" << key << "]:\t\t" << output << std::endl;
-    file.close();
+  std::ofstream file(logVars, std::fstream::app);
+  file << "[" << key << "]:\t\t" << output << std::endl;
+  file.close();
 }
 
 void Logging::print(std::string output, int color) const {
-    std::string s = "\033[";  ///< Escape code for Ascii color code
-    s += "38;2;";  ///< Escape code for the r;g;b color code to follow
-    s += COLOR_CODE[color];
-    s += "m";
+  std::string s = "\033["; ///< Escape code for Ascii color code
+  s += "38;2;";            ///< Escape code for the r;g;b color code to follow
+  s += COLOR_CODE[color];
+  s += "m";
 
-    s += output;
+  s += output;
 
-    s += "\033[;0m";  ///< Reset code
-    std::cout << s << std::endl;
+  s += "\033[;0m"; ///< Reset code
+  std::cout << s << std::endl;
 }
-
